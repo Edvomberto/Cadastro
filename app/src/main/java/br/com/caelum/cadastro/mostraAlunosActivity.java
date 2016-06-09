78a7ab6db8ded7abd28bc11afda6ab38cf441c33
honorato <e.honorato@kconsulting.com.br>
2016-06-09 12:57:27 -0300

    Inclusão do EditText de pesquisa no Mapa

--- app/src/main/java/br/com/caelum/cadastro/mostraAlunosActivity.java
+++ app/src/main/java/br/com/caelum/cadastro/mostraAlunosActivity.java
@@ -1,7 +1,14 @@
 package br.com.caelum.cadastro;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -23,6 +30,9 @@ public class mostraAlunosActivity extends FragmentActivity implements OnMapReady
 
     private GoogleMap mMap;
 
+    MarkerOptions markerOptions;
+    LatLng latLng;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +41,82 @@ public class mostraAlunosActivity extends FragmentActivity implements OnMapReady
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Button btn_find = (Button) findViewById(R.id.btn_find);
+
+        // Defining button click event listener for the find button
+
+        btn_find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Getting reference to EditText to get the user input location
+                EditText etLocation = (EditText) findViewById(R.id.et_location);
+
+                // Getting user input location
+                String location = etLocation.getText().toString();
+
+                if (location != null && !location.equals("")) {
+                    new GeocoderTask().execute(location);
+                }
+            }
+        });
+
+    }
+
+
+    // An AsyncTask class for accessing the GeoCoding Web Service
+    private class GeocoderTask extends AsyncTask<String, Void, List<Address>> {
+
+        @Override
+        protected List<Address> doInBackground(String... locationName) {
+            // Creating an instance of Geocoder class
+            Geocoder geocoder = new Geocoder(getBaseContext());
+            List<Address> addresses = null;
+
+            try {
+                // Getting a maximum of 3 Address that matches the input text
+                addresses = geocoder.getFromLocationName(locationName[0], 3);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return addresses;
+        }
+
+        @Override
+        protected void onPostExecute(List<Address> addresses) {
+
+            if(addresses==null || addresses.size()==0){
+                Toast.makeText(getBaseContext(), "No Location found", Toast.LENGTH_SHORT).show();
+            }
+
+            // Clears all the existing markers on the map
+            mMap.clear();
+
+            // Adding Markers on Google Map for each matching address
+            for(int i=0;i<addresses.size();i++){
+
+                Address address = (Address) addresses.get(i);
+
+                // Creating an instance of GeoPoint, to display in Google Map
+                latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+                String addressText = String.format("%s, %s",
+                        address.getMaxAddressLineIndex() > 0 ? address.getAddressLine(0) : "",
+                        address.getCountryName());
+
+                markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title(addressText);
+
+                mMap.addMarker(markerOptions);
+
+                // Locate the first location
+                if(i==0)
+                    //mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                centralizaNo(latLng);
+            }
+        }
     }
 
 
@@ -78,7 +164,7 @@ public class mostraAlunosActivity extends FragmentActivity implements OnMapReady
 
     public void centralizaNo(LatLng coordenada)
     {
-        CameraUpdate camera = CameraUpdateFactory.newLatLngZoom(coordenada, 11);
+        CameraUpdate camera = CameraUpdateFactory.newLatLngZoom(coordenada, 15);
         mMap.moveCamera(camera);
     }
 
